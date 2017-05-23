155efd79ada142a5dc2b1c44e37d2752b93a451c
Bryan Wigianto <bwigianto@gmail.com>
2017-05-23 00:03:42 -0500

    fix typo

--- src/questions/mod.js
+++ src/questions/mod.js
@@ -4,5 +4,5 @@ export function gen() {
   var a = randInt(1000, 10000);
   var b = randInt(1, 20);
   var ans = a%b;
-  return {question: a + '/' + b + 'has a remainder of' , answer: ans};
+  return {question: a + '/' + b + ' has a remainder of' , answer: ans};
 }
